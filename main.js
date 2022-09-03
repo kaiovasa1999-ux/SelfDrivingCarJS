d5e85855b39ca8a48a4c8385254859c7ccfe5846
Veselin Valkanov <veselin.d.valkanov@gmail.com>
2022-09-03 14:45:40 +0300

    Car class was impleneted

---
+++ main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("myCanvas")
+canvas.height=window.innerHeight;
+canvas.width=400;
+
+const ctx =canvas.getContext("2d");
+const car = new Car(200,100,30,50);
+
+car.draw(ctx);
