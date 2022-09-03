42c0d9768af8dd9d8d1ed11d2fb9885db00e144f
Veselin Valkanov <veselin.d.valkanov@gmail.com>
2022-09-03 15:13:39 +0300

    make the rectangle/car movable

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-const canvas = document.getElementById("myCanvas")
-canvas.height=window.innerHeight;
+const canvas=document.getElementById("myCanvas");
 canvas.width=400;
 
 const ctx =canvas.getContext("2d");
 const car = new Car(200,100,30,50);
 
-car.draw(ctx);
-
 animate()
 
 function animate(){
   car.update();
+  canvas.height=window.innerHeight;
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
