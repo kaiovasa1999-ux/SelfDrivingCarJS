e01a04f35d386b01a6bd62084544bf51095dc0a8
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-09-11 14:07:05 +0300

    Initial road Class

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const canvas=document.getElementById("myCanvas");
 canvas.width=400;
 
 const ctx =canvas.getContext("2d");
+const road = new Road(canvas.width/2,canvas.width);
 const car = new Car(200,100,30,50);
 
 animate()
@@ -9,6 +10,7 @@ animate()
 function animate(){
   car.update();
   canvas.height=window.innerHeight;
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
