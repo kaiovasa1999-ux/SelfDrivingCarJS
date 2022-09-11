b1aef61e59f6a512efef94599c1c287d5b4e4570
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-09-11 13:34:17 +0300

    fix the controllers for left and right

--- car.js
+++ car.js
@@ -8,8 +8,9 @@ class Car{
 
     this.speed =0;
     this.acceleration =0.25;
-    this.maxSpeed =2;
+    this.maxSpeed =1;
     this.friction = 0.015;
+    this.angle = 0;
     this.contrs = new Controls();
   }
 
@@ -32,21 +33,28 @@ class Car{
     if(this.speed < 0){
       this.speed += this.friction;
     }
-    if(this.contrs.right){
-      this.x +=2;
+    if(this.speed !=0){
+      const flip =this.speed>0?1:1;
+      if(this.contrs.left){
+        this.angle += 0.01 * flip;
+      }
+      if(this.contrs.right){
+        this.angle -= 0.01 * flip;
+      }
     }
-    if(this.contrs.left){
-      this.x -=2;
-    }
-    this.y -= this.speed
+    this.x -=Math.sin(this.angle)*this.speed;
+    this.y -= Math.cos(this.angle)* this.speed;
   }
 
 
   draw(ctx){
+    ctx.save();
+    ctx.translate(this.x,this.y);
+    ctx.rotate(-this.angle);
     ctx.beginPath();
     ctx.rect(
-      this.x - this.width/2,
-      this.y - this.height/2,
+       - this.width/2,
+      - this.height/2,
       this.width,
       this.height,
     )
