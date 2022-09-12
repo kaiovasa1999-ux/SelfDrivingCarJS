ea45c0c98ddd9229e34a8e672c42f955058141c7
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-09-12 11:31:00 +0300

    remove road from main js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const canvas=document.getElementById("myCanvas");
 canvas.width=400;
 
 const ctx =canvas.getContext("2d");
-const road = new Road(canvas.width/2,canvas.width)
 const car = new Car(200,100,30,50);
 
 animate()
@@ -10,7 +9,6 @@ animate()
 function animate(){
   car.update();
   canvas.height=window.innerHeight;
-  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
