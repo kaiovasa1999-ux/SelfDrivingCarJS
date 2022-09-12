7bd23263befb729e50b57f2fb1a5000a243d3b72
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2022-09-12 13:13:26 +0300

    Liner linear interpolation formula implemented in the road class

--- road.js
+++ road.js
@@ -16,14 +16,16 @@ class Road{
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
-        ctx.beginPath();
-        ctx.moveTo(this.left,this.top);
-        ctx.lineTo(this.left,this.bottom);
-        ctx.stroke();
+        for(let i=0;i<=this.laceCount;i++){
+            const x = lerp(this.left,this.right, i/this.laceCount)
 
-        ctx.beginPath();
-        ctx.moveTo(this.right,this.top);
-        ctx.lineTo(this.right,this.bottom);
-        ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(x,this.top);
+            ctx.lineTo(x,this.bottom);
+            ctx.stroke();
+        }
     }
+}
+function lerp(A,B,t) {
+        return A+(B-A)*t;
 }
